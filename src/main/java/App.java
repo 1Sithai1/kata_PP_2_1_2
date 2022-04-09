8ccdca23453d0aac96dc2e878f42a5121d8280c8
Sithai <trupachello@gmail.com>
2022-04-09 13:09:13 +0300

    Релиз

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println("Сравнение ссылок HelloWorld: " + (bean1 == bean2));
+
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Сравнение ссылок Cat: " + (catBean1 == catBean2));
+//        System.out.println(bean1.getMessage());
     }
 }
